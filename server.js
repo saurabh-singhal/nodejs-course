910e00f94cdec79155891cbb50e4e6555b62da48
saurabh singhal <saurabh.singhal333@gmail.com>
2017-07-03 00:19:56 +0530

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express= require('express');
 const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -27,4 +30,4 @@ app.get('/about',function(req,res)
   });
 });
 
-app.listen(3000);
+app.listen(port);
